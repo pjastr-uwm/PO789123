f3f0e0d017d12de21b4f3bbfba6c9bf4df342dd6
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-11-07 15:29:18 +0100

    ulamek

---
+++ cw4/zad_ulamek/liczby.py
@@ -0,0 +1,20 @@
+from math import gcd
+
+
+class Wymierna:
+    p: int
+    q: int
+
+    def __init__(self, p: int, q: int) -> None:
+        self.p = p
+        self.q = q
+        if gcd(self.p, self.q) != 1:
+            self.p //= gcd(p, q)
+            self.q //= gcd(p, q)
+
+        if self.q < 0:
+            self.p *= -1
+            self.q *= -1
+
+    def __repr__(self):
+        return f"({self.p}, {self.q})"
---
+++ cw4/zad_ulamek/main.py
@@ -0,0 +1,10 @@
+from liczby import Wymierna
+
+
+def main():
+    w1: Wymierna = Wymierna(-6, -4)
+    print(w1)
+
+
+if __name__ == "__main__":
+    main()
