be2514677a522b8938aa26e01a76138b7113ec80
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-11-14 14:20:11 +0100

    up

--- cw4/zad_ulamek/liczby.py
+++ cw4/zad_ulamek/liczby.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from math import gcd
 
 
@@ -21,3 +22,15 @@ class Wymierna:
 
     def __repr__(self):
         return f"({self.p}, {self.q})"
+
+    def get_licznik(self) -> int:
+        return self.p
+
+    def get_mianownik(self) -> int:
+        return self.q
+
+    def __float__(self) -> float:
+        return self.p/self.q
+
+    def __add__(self, other: Wymierna) -> Wymierna:
+        return Wymierna(self.p * other.q+self.q * other.p, self.q * other.q)
--- cw4/zad_ulamek/main.py
+++ cw4/zad_ulamek/main.py
@@ -2,8 +2,16 @@ from liczby import Wymierna
 
 
 def main() -> None:
-    w1: Wymierna = Wymierna(-6, 3)
+    w1: Wymierna = Wymierna(-6, -3)
     print(w1)
+    print(w1.get_licznik())
+    print(w1.get_mianownik())
+    print(w1.__float__())
+    print(float(w1))
+    w2: Wymierna = Wymierna(1, 3)
+    print(float(w2))
+    w3: Wymierna = Wymierna(2, 3)
+    print(w2+w3)
 
 
 if __name__ == "__main__":
