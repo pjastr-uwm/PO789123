769f925a40a6ad9eac794a4f54f2f94728b4be3f
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-11-28 14:18:43 +0100

    up

--- cw6/zad6Osoba/test_osoba.py
+++ cw6/zad6Osoba/test_osoba.py
@@ -1,4 +1,4 @@
-from osoba import  Osoba
+from osoba import  Osoba, Pracownik
 
 def main() -> None:
     o1: Osoba = Osoba("Kowalski",2000)
@@ -11,6 +11,16 @@ def main() -> None:
     print(Osoba.get_ile())
     Osoba.zwieksz_pobory(50)
     print(Osoba.get_ile())
+    ## komendy z polecenia
+    print(Osoba.get_ile())
+    o2: Osoba = Osoba("Nowak", 1995)
+    print(o2.get_ile())
+    p1: Pracownik = Pracownik("Tuwim", 1900, 1920, 2000)
+    print(p1.get_ile())
+    del o2
+    del p1
+    # print(p1)
+    print(Pracownik.get_ile())
 
 
 if __name__ == "__main__":
