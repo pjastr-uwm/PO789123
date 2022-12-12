ef7e2a5c673aa8420fccb711eb0900cb4403daa3
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-12-12 14:04:13 +0100

    1c

--- cw9/src/pl/uwm/wmii/lab9/zad1c.java
+++ cw9/src/pl/uwm/wmii/lab9/zad1c.java
@@ -1,4 +1,19 @@
 package pl.uwm.wmii.lab9;
 
+import java.util.Scanner;
+
 public class zad1c {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj ile liczb chcesz wpisac");
+        int n = input.nextInt();
+        double temp=0;
+        for(int i=0; i<n; i++)
+        {
+            System.out.printf("Podaj liczbe nr %d\n: ",i+1);
+            temp+=Math.abs(input.nextDouble());
+        }
+        System.out.printf("Wynik: %f",temp);
+    }
 }
