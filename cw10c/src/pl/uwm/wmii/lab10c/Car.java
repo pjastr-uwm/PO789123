6b601c8ecf4ad5451ebc3013e18278e7b7a7e25b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-12-19 15:25:52 +0100

    test

---
+++ cw10c/src/pl/uwm/wmii/lab10c/Car.java
@@ -0,0 +1,6 @@
+package pl.uwm.wmii.lab10c;
+
+public class Car {
+    String brand;
+    int year;
+}
---
+++ cw10c/src/pl/uwm/wmii/lab10c/TestCar.java
@@ -0,0 +1,9 @@
+package pl.uwm.wmii.lab10c;
+
+public class TestCar {
+
+    public static void main(String[] arg){
+        Car c1 = new Car();
+        c1.brand = "Fiat";
+    }
+}
