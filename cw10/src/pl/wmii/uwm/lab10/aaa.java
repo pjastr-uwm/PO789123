e15cc8acbea81fec49d8f33a5ac8ca2b02f0a907
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-12-19 14:46:54 +0100

    e

---
+++ cw10/src/pl/wmii/uwm/lab10/aaa.java
@@ -0,0 +1,4 @@
+package pl.wmii.uwm.lab10;
+
+public class aaa {
+}
---
+++ cw10b/src/pl/lab10/Zad1a.java
@@ -0,0 +1,35 @@
+package pl.lab10;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Zad1a {
+
+    public static void Main(String[] args)
+    {
+        System.out.println("Podaj ile liczb chcesz wylosować");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int tab[] = new int[n];
+        Random r = new Random();
+        for (int j = 0; j < tab.length; ++j) {
+            tab[j] = r.nextInt(1999)-999;
+        }
+        int counter=0; // do parzystych
+        int counter2=0; //dla nieparzystych
+        for(int j=0;j<tab.length;j++)
+        {
+            if (tab[j] %2 ==0)
+            {
+                counter++;
+            }
+            else
+            {
+                counter2++;
+            }
+        }
+        System.out.printf("Parzyste: %d%n", counter);
+        System.out.printf("Nieparzyste: %d%n", counter2);
+    }
+}
+
