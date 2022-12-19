85cfeedd2eb48f5ff5fc32eff5b13104eb871855
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-12-19 14:50:00 +0100

    up

--- cw10b/src/pl/lab10/Zad1a.java
+++ cw10b/src/pl/lab10/Zad1a.java
@@ -5,13 +5,14 @@ import java.util.Random;
 
 public class Zad1a {
 
-    public static void Main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Podaj ile liczb chcesz wylosować");
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int tab[] = new int[n];
         Random r = new Random();
+        r.setSeed(455422);
         for (int j = 0; j < tab.length; ++j) {
             tab[j] = r.nextInt(1999)-999;
         }
