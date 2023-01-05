12b08ce0561fb23bd37dcecc38eab5416fa52588
pjastr <24464854+pjastr@users.noreply.github.com>
2023-01-05 14:44:39 +0100

    osoba2

--- LIsta11Cz1/src/pl/uwm/wmii/Nauczyciel.java
+++ LIsta11Cz1/src/pl/uwm/wmii/Nauczyciel.java
@@ -1,4 +1,33 @@
 package pl.uwm.wmii;
 
-class Nauczyciel{
+import java.util.Objects;
+
+class Nauczyciel extends Osoba{
+    public Nauczyciel(String nazwisko, int rokUrodzenia, double pensja) {
+        super(nazwisko, rokUrodzenia);
+        this.pensja = pensja;
+    }
+
+    @Override
+    public String toString() {
+        return "Nauczyciel{" + "nazwisko='" + getNazwisko() + '\'' +
+                ", rokUrodzenia=" + getRokUrodzenia() +
+                ", pensja=" + this.pensja +
+                '}';
+    }
+
+    public double getPensja() {
+        return pensja;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!super.equals(otherObject)) {
+            return false;
+        }
+        Nauczyciel other = (Nauczyciel) otherObject;
+        return this.pensja == other.pensja;
+    }
+
+    private double pensja;
 }
--- LIsta11Cz1/src/pl/uwm/wmii/TestOsoba.java
+++ LIsta11Cz1/src/pl/uwm/wmii/TestOsoba.java
@@ -8,5 +8,17 @@ class TestOsoba {
         System.out.println(o1);
         Osoba o2 = new Osoba("Kowalski", 2001);
         System.out.println(o1.equals(o2));
+        Nauczyciel n1 = new Nauczyciel("Nowak", 1990, 4000.00);
+        System.out.println(n1);
+        Osoba n2 = new Nauczyciel("Nowak", 1990, 4000.00);
+        System.out.println(n2);
+        Nauczyciel n3 = new Nauczyciel("Nowak", 1990, 4000.00);
+        System.out.println(n1.equals(n3));
+        System.out.println(n2.equals(n3));
+        System.out.println(n3.equals(n2));
+        Osoba o4 = new Osoba("Nowak", 1990);
+        System.out.println(n3.equals(o4));
+        System.out.println(n2.equals(o4));
+        System.out.println(o4.equals(n3));
     }
 }
