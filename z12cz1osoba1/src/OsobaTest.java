1065c8c9638cdb9d4b25417b8b8f4eb72da45071
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-01-23 13:52:26 +0100

    p2

--- z12cz1osoba1/src/OsobaTest.java
+++ z12cz1osoba1/src/OsobaTest.java
@@ -1,2 +1,30 @@
+import pl.uwm.wmii.kmmi.nazwisko.Osoba;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class OsobaTest {
+
+    public static void main(String[] args)
+    {
+        Osoba[] grupa ={
+                new Osoba("Kowalski", LocalDate.of(2000,1,1)),
+                new Osoba("Kowalski", LocalDate.of(2003,3,2)),
+                new Osoba("Nowak", LocalDate.of(1999,11,21)),
+                new Osoba("Nowak", LocalDate.of(2010,2,21)),
+                new Osoba("Wileński", LocalDate.of(2000,1,1))
+        };
+
+        System.out.println("Przed");
+        for(Osoba elem: grupa)
+        {
+            System.out.println(elem);
+        }
+        Arrays.sort(grupa);
+        System.out.println("Po");
+        for(Osoba elem: grupa)
+        {
+            System.out.println(elem);
+        }
+    }
 }
